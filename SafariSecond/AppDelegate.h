a414d2b4d7ec6ad12c19815eff1533d9c21f5c32
Valak12 <Kohnechi@gmail.com>
2014-10-22 15:17:23 -0700

    Initial Commit

---
+++ SafariSecond/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariSecond
+//
+//  Created by Vala Kohnechi on 10/22/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SafariSecond/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafariSecond
+//
+//  Created by Vala Kohnechi on 10/22/14.
+//  Copyright (c) 2014 Vala Kohnechi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
