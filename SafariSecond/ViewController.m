ebaec2e8d003498d53ce8d28b346ac39ba8ddc2d
Andrew Liu <dieseliu321@gmail.com>
2014-10-23 16:11:41 -0700

    move view and texfield

--- SafariSecond/ViewController.m
+++ SafariSecond/ViewController.m
@@ -12,6 +12,7 @@
 @property (strong, nonatomic) IBOutlet UIWebView *webView;
 @property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UIView *buttonView;
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *networkActivityIndicator;
 @property (strong, nonatomic) IBOutlet UIButton *backButton;
 @property (strong, nonatomic) IBOutlet UIButton *forwardButton;
@@ -22,23 +23,8 @@
 
 - (void)updateButton
 {
-    if (self.webView.canGoBack)
-    {
-        self.backButton.enabled = YES;
-    }
-    else
-    {
-        self.backButton.Enabled = NO;
-    }
-
-    if (self.webView.canGoForward)
-    {
-        self.forwardButton.Enabled = YES;
-    }
-    else
-    {
-        self.forwardButton.enabled = NO;
-    }
+    self.backButton.enabled = self.webView.canGoBack;
+    self.forwardButton.enabled = self.webView.canGoForward;
     //display current URL
     self.urlTextField.text = [self.webView stringByEvaluatingJavaScriptFromString : @"window.location.href"];
     //display current page title
@@ -78,9 +64,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.backButton.Enabled = NO;
-    self.forwardButton.Enabled = NO;
-    [self loadURL : @"http://mobilemakers.co"];
+    [self loadURL : @"http://www.google.com"];
     self.webView.scrollView.delegate = self;
 }
 
@@ -102,7 +86,6 @@
      }
      ];
 }
-
 
 //navigate back to previous page
 - (IBAction)onBackButtonPressed : (id)sender
@@ -143,20 +126,26 @@
     if (scrollView.contentOffset.y <= 0)
     {
         self.urlTextField.alpha = 0.4;
-        [UIView animateWithDuration : .8 animations : ^
+        self.buttonView.alpha = 0.4;
+        [UIView animateWithDuration : 0.8 animations : ^
          {
              self.urlTextField.alpha = 1;
-             self.urlTextField.frame = CGRectMake(self.urlTextField.frame.origin.x , 60 , self.urlTextField.frame.size.width , self.urlTextField.frame.size.height);
+             self.buttonView.alpha = 1;
+             self.urlTextField.frame = CGRectMake(self.webView.frame.origin.x+16 , self.webView.frame.origin.y+5 , self.webView.frame.size.width-32 , self.urlTextField.frame.size.height);
+             self.buttonView.frame =CGRectMake(self.webView.frame.origin.x , self.webView.frame.size.height+15 , self.webView.frame.size.width , self.buttonView.frame.size.height);
          }
         ];
     }
     else
     {
         self.urlTextField.alpha = 1;
+        self.buttonView.alpha = 1;
         [UIView animateWithDuration : 1.2 animations : ^
          {
              self.urlTextField.alpha = 0.4;
-             self.urlTextField.frame = CGRectMake(self.urlTextField.frame.origin.x , -30 , self.urlTextField.frame.size.width , self.urlTextField.frame.size.height);
+             self.buttonView.alpha = 0.4;
+             self.urlTextField.frame = CGRectMake(self.webView.frame.origin.x+16 , self.webView.frame.origin.y-90 , self.webView.frame.size.width-32 , self.urlTextField.frame.size.height);
+             self.buttonView.frame =CGRectMake(self.webView.frame.origin.x , self.webView.frame.size.height+55 , self.webView.frame.size.width , self.buttonView.frame.size.height);
          }
         ];
     }
